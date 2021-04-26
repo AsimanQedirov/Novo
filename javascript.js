6b3c3360487e050f5dd05afbfba5ddad52534628
unknown <asiman.gadirov.1998@gmail.com>
2021-04-27 00:46:27 +0400

    novo

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// var collapseDiv = document.getElementById('navbarSupportedContent')
+// let windowWidth = window.innerWidth
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     responsive()
+// });
+// window.addEventListener('resize',()=>{
+//     responsive()
+// })
+// const responsive = ()=>{
+//     if(windowWidth < 770){
+//         collapseDiv.className = "collapse navbar-collapse"
+//     }
+//     else{
+//         collapseDiv.className="d-flex justify-content-end"
+//     }
+// }
